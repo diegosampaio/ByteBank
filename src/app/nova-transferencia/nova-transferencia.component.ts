ba794caf559a548fa0344a11b192c0081b576bbc
diegosampaio <diego.estaleiro@gmail.com>
2022-04-12 11:46:54 -0300

    janela de transferencia de valores

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,27 @@
-import { Component } from "@angular/core";
-
+import { Component, Output, EventEmitter } from "@angular/core";
 @Component({
-  selector: 'nova-transferencia',
+  selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss']
 })
 
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('transferência solicitada com sucesso!');
+
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
